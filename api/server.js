39a053ce0f82a9d16f65eeb46a207044080592a8
kato hawkins <kristhahawkins@icloud.com>
2021-06-08 22:58:50 -0400

    middleware complete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const helmet = require("helmet")
+const morgan = require("morgan")
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(helmet())
+server.use(morgan("dev"))
+server.use(express.json())
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
